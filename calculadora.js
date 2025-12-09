13a78278640ee31e6784b6d48ee09a14b1671232
Clauidio Medellin <claudiomedellin@tecnomotum.com>
2025-12-08 18:10:49 -0600

    "Feat: Implementada función de suma (Fase Verde)"

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,24 @@
-function sumar(a, b) {
-    return a + b;
-}
+// calculadora.test.js
+const { sumar, restar, multiplicar, dividir } = require('./calculadora');
 
-module.exports = { sumar };
+describe('Pruebas de la Calculadora', () => {
+    test('Debe sumar dos números reales correctamente', () => {
+        expect(sumar(2, 2)).toBe(4);
+        expect(sumar(5.5, 2.1)).toBe(7.6);
+    });
+
+    test('Debe restar dos números reales correctamente', () => {
+        expect(restar(5, 2)).toBe(3);
+        expect(restar(2, 5)).toBe(-3);
+    });
+
+    test('Debe multiplicar dos números reales correctamente', () => {
+        expect(multiplicar(4, 2)).toBe(8);
+        expect(multiplicar(2.5, 2)).toBe(5);
+    });
+
+    test('Debe dividir dos números reales correctamente', () => {
+        expect(dividir(10, 2)).toBe(5);
+        expect(dividir(10, 4)).toBe(2.5);
+    });
+});
